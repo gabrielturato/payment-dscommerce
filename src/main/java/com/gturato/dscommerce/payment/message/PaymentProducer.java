1f13015b8211c8fda1dc13bd25f2f9488025020b
Gabriel Turato <gabrielturato@outlook.com>
2025-05-14 16:23:24 -0300

    Kafka topic configuration

--- src/main/java/com/gturato/dscommerce/payment/message/PaymentProducer.java
+++ src/main/java/com/gturato/dscommerce/payment/message/PaymentProducer.java
@@ -1,4 +1,24 @@
 package com.gturato.dscommerce.payment.message;
 
+import com.gturato.dscommerce.payment.dto.PaymentDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
 public class PaymentProducer {
+
+    @Autowired
+    private KafkaTemplate<String, PaymentDTO> kafkaTemplate;
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
+
+    public void sendPayment(PaymentDTO paymentDTO) {
+        kafkaTemplate.send(topicName, paymentDTO);
+    }
+
 }
