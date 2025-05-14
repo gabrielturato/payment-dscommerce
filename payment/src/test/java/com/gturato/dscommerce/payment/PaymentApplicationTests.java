3d8036a42d8f9248be98341fe335361bd3167c73
Gabriel Turato <gabrielturato@outlook.com>
2025-05-14 15:33:51 -0300

    First commit

---
+++ payment/src/test/java/com/gturato/dscommerce/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gturato.dscommerce.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
